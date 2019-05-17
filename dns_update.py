30bc6e8370144f59e8faf05adaa13b49cc41886b
Tobi <tobipristupin@gmail.com>
2019-05-17 18:09:16 -0400

    updated dns script

--- dns_update.py
+++ dns_update.py
@@ -3,9 +3,16 @@ import datetime
 path = "/var/www/html/"
 ip = get('https://api.ipify.org').text
 user, password = "", ""
+open(path + "dns_log.txt", "w").close() #Clear contents of log file
+
+def request_and_log(user, password, ip, domain):
+    req = get("https://{user}:{password}@domains.google.com/nic/update?hostname={domain}&myip={ip}".format(user=user, password=password, ip=ip, domain=domain))
+    with open(path + "dns_log.txt", "a") as f:
+        f.write(req.text + " " + str(datetime.datetime.now()) + "\n")
+
 with open(path + "credentials.txt") as f:
-	user, password = f.readline().strip(), f.readline().strip()
-req = get("https://{user}:{password}@domains.google.com/nic/update?hostname=pristu.dev&myip={ip}".format(user=user, password=password, ip=ip))
-with open(path + "dns_log.txt", "w") as f:
-	f.write(req.text + " " + str(datetime.datetime.now()) + "\n")
+    user, password = f.readline().strip().split("*")
+    request_and_log(user, password, ip, "www.pristu.dev")
+    user, password = f.readline().strip().split("*")
+    request_and_log(user, password, ip, "pristu.dev")
 
