b3e29606de4cca161a6fe1d74cc728f55784530a
Tobi <tobipristupin@gmail.com>
2019-05-17 17:35:32 -0400

    compressed images

--- dns_update.py
+++ dns_update.py
@@ -1,10 +1,11 @@
 from requests import get
 import datetime
+path = "/var/www/html/"
 ip = get('https://api.ipify.org').text
 user, password = "", ""
-with open("credentials.txt") as f:
+with open(path + "credentials.txt") as f:
 	user, password = f.readline().strip(), f.readline().strip()
-req = get("https://{user}:{password}@domains.google.com/nic/update?hostname=www.pristu.dev&myip={ip}".format(user=user, password=password, ip=ip))
-with open("dns_log.txt", "w") as f:
+req = get("https://{user}:{password}@domains.google.com/nic/update?hostname=pristu.dev&myip={ip}".format(user=user, password=password, ip=ip))
+with open(path + "dns_log.txt", "w") as f:
 	f.write(req.text + " " + str(datetime.datetime.now()) + "\n")
 
